3bc0f61f038b87126402458550cc976d521ef55b
Bounty556 <jacobmayday@gmail.com>
2021-07-11 21:00:38 -0600

    Changed QuadTree from template to concrete

---
+++ Pong/Engine/src/Structures/QuadTree.cpp
@@ -0,0 +1,135 @@
+#include "QuadTree.h"
+
+namespace Soul
+{
+	QuadTree::QuadTree(f32 x, f32 y, f32 width, f32 height, u32 maxChildren) :
+	m_MaxChildren(maxChildren),
+	m_Storage(m_MaxChildren + 1),
+	m_Children(nullptr),
+	m_Position(x, y),
+	m_Area(width, height)
+	{
+	}
+
+	QuadTree::QuadTree(QuadTree&& other) noexcept :
+	m_MaxChildren(other.m_MaxChildren),
+	m_Storage(std::move(other.m_Storage)),
+	m_Children(other.m_Children),
+	m_Position(other.m_Position),
+	m_Area(other.m_Area)
+	{
+	other.m_Children = nullptr;
+	}
+
+	QuadTree::~QuadTree()
+	{
+	if (m_Children)
+		MemoryManager::FreeMemory(m_Children);
+	}
+
+	QuadTree& QuadTree::operator=(QuadTree&& other) noexcept
+	{
+	m_MaxChildren = other.m_MaxChildren;
+	m_Storage = std::move(other.m_Storage);
+	m_Children = other.m_Children;
+	m_Position = other.m_Position;
+	m_Area = other.m_Area;
+	other.m_Children = nullptr;
+	}
+
+	QuadTree* QuadTree::Insert(Node* node, sf::Vector2f area)
+	{
+		// Find smallest tree this will fit in
+		if (m_Children)
+		{
+			if (AABBIsInAABB(node->getPosition(), area, m_Children[0].m_Position, m_Children[0].m_Area))
+				m_Children[0].Insert(node, area);
+			else if (AABBIsInAABB(node->getPosition(), area, m_Children[1].m_Position, m_Children[1].m_Area))
+				m_Children[1].Insert(node, area);
+			else if (AABBIsInAABB(node->getPosition(), area, m_Children[2].m_Position, m_Children[2].m_Area))
+				m_Children[2].Insert(node, area);
+			else if (AABBIsInAABB(node->getPosition(), area, m_Children[3].m_Position, m_Children[3].m_Area))
+				m_Children[3].Insert(node, area);
+			else
+				AddToStorage(node, area);
+		}
+		else
+		{
+			AddToStorage(node, area);
+		}
+	}
+
+	QuadTree* QuadTree::Insert(QuadTreeItem quadTreeItem)
+	{
+		sf::Vector2f position = quadTreeItem.node->getPosition();
+		sf::Vector2f area = quadTreeItem.area;
+
+		// Find smallest tree this will fit in
+		if (m_Children)
+		{
+			if (AABBIsInAABB(position, area, m_Children[0].m_Position, m_Children[0].m_Area))
+				m_Children[0].Insert(quadTreeItem);
+			else if (AABBIsInAABB(position, area, m_Children[1].m_Position, m_Children[1].m_Area))
+				m_Children[1].Insert(quadTreeItem);
+			else if (AABBIsInAABB(position, area, m_Children[2].m_Position, m_Children[2].m_Area))
+				m_Children[2].Insert(quadTreeItem);
+			else if (AABBIsInAABB(position, area, m_Children[3].m_Position, m_Children[3].m_Area))
+				m_Children[3].Insert(quadTreeItem);
+			else
+				AddToStorage(quadTreeItem);
+		}
+		else
+		{
+			AddToStorage(quadTreeItem);
+		}
+	}
+
+	QuadTree* QuadTree::Move(QuadTree* origin, Node* node)
+	{
+		sf::Vector2f area = {};
+		for (u32 i = 0; i < m_Storage.Count(); ++i)
+			if (m_Storage[i].node == node)
+				area = m_Storage[i].area;
+
+		// TODO: Let's check to see if this has passed our boundaries first
+		if (!AABBIsInAABB(node->getPosition(), area, m_Position, m_Area))
+		{
+			QuadTreeItem quadTreeItem = origin->Remove(origin, node);
+			Insert(quadTreeItem);
+			FlattenTree();
+		}
+	}
+
+	QuadTree::QuadTreeItem QuadTree::Remove(QuadTree* origin, Node* node)
+	{
+		QuadTreeItem found = {};
+		for (u32 i = 0; i < m_Storage.Count(); ++i)
+			if (m_Storage[i].node == node)
+				return *m_Storage.Remove(i);
+	}
+
+	Vector<Node*> QuadTree::GetNodes(sf::Vector2f position, sf::Vector2f area)
+	{
+		// TODO:
+	}
+
+	void QuadTree::AddToStorage(Node* node, sf::Vector2f area)
+	{
+		// TODO:
+	}
+
+	void QuadTree::AddToStorage(QuadTreeItem quadTreeItem)
+	{
+		// TODO:
+	}
+
+	void QuadTree::FlattenTree()
+	{
+		// TODO:
+	}
+
+	void QuadTree::SplitTree()
+	{
+		// TODO:
+	}
+}
--- Pong/Engine/src/Structures/QuadTree.h
+++ Pong/Engine/src/Structures/QuadTree.h
@@ -2,139 +2,51 @@
 
 #include <Defines.h>
 #include <Structures/Vector.h>
+#include <Physics/CollisionChecks.h>
+#include <Nodes/Node.h>
 
 #include <SFML/System/Vector2.hpp>
 
 namespace Soul
 {
-	template <class T>
 	class QuadTree
 	{
 	private:
-		template <class T>
-		struct Node
+		struct QuadTreeItem
 		{
-			T* node;
-			sf::Vector2f position;
+			Node* node;
 			sf::Vector2f area;
 		};
 
 	public:
 		QuadTree(f32 x, f32 y, f32 width, f32 height, u32 maxChildren);
 
-		QuadTree(const QuadTree<T>&) = delete;
-		QuadTree(QuadTree<T>&& other) noexcept;
+		QuadTree(const QuadTree&) = delete;
+		QuadTree(QuadTree&& other) noexcept;
 
 		~QuadTree();
 
-		QuadTree<T>& operator=(const QuadTree<T>&) = delete;
-		QuadTree<T>& operator=(QuadTree<T>&& other) noexcept;
+		QuadTree& operator=(const QuadTree&) = delete;
+		QuadTree& operator=(QuadTree&& other) noexcept;
 
-		QuadTree<T>* Insert(T* node, sf::Vector2f position, sf::Vector2f area);
-		QuadTree<T>* Move(QuadTree* origin, T* node);
-		void Remove(QuadTree* origin, T* node);
+		QuadTree* Insert(Node* node, sf::Vector2f area);
+		QuadTree* Insert(QuadTreeItem quadTreeItem);
+		QuadTree* Move(QuadTree* origin, Node* node);
+		QuadTreeItem Remove(QuadTree* origin, Node* node);
 
-		Vector<T*> GetNodes(sf::Vector2f position, sf::Vector2f area);
+		Vector<Node*> GetNodes(sf::Vector2f position, sf::Vector2f area);
 
 	private:
-		void AddToStorage(T* node, sf::Vector2f position, sf::Vector2f area);
+		void AddToStorage(Node* node, sf::Vector2f area);
+		void AddToStorage(QuadTreeItem quadTreeItem);
 		void FlattenTree();
 		void SplitTree();
 
 	private:
 		u32 m_MaxChildren;
-		Vector<Node<T>> m_Storage;
-		QuadTree<T>* m_Children;
+		Vector<QuadTreeItem> m_Storage;
+		QuadTree* m_Children;
 		sf::Vector2f m_Position;
 		sf::Vector2f m_Area;
 	};
-
-	template <class T>
-	QuadTree<T>::QuadTree(f32 x, f32 y, f32 width, f32 height, u32 maxChildren) :
-		m_MaxChildren(maxChildren),
-		m_Storage(m_MaxChildren + 1),
-		m_Children(nullptr),
-		m_Position(x, y),
-		m_Area(width, height)
-	{
-	}
-
-	template <class T>
-	QuadTree<T>::QuadTree(QuadTree&& other) noexcept :
-		m_MaxChildren(other.m_MaxChildren),
-		m_Storage(std::move(other.m_Storage)),
-		m_Children(other.m_Children),
-		m_Position(other.m_Position),
-		m_Area(other.m_Area)
-	{
-		other.m_Children = nullptr;
-	}
-
-	template <class T>
-	QuadTree<T>::~QuadTree()
-	{
-		if (m_Children)
-			MemoryManager::FreeMemory(m_Children);
-	}
-
-	template <class T>
-	QuadTree<T>& QuadTree<T>::operator=(QuadTree<T>&& other) noexcept
-	{
-		m_MaxChildren = other.m_MaxChildren;
-		m_Storage = std::move(other.m_Storage);
-		m_Children = other.m_Children;
-		m_Position = other.m_Position;
-		m_Area = other.m_Area;
-		other.m_Children = nullptr;
-	}
-
-	template <class T>
-	QuadTree<T>* QuadTree<T>::Insert(T* node, sf::Vector2f position, sf::Vector2f area)
-	{
-		// Find smallest tree this will fit in
-		if (m_Children)
-		{
-			if ()
-		}
-		else
-		{
-			AddToStorage(node, position, area);
-		}
-	}
-
-	template <class T>
-	QuadTree<T>* QuadTree<T>::Move(QuadTree<T>* origin, T* node)
-	{
-		// TODO:
-	}
-
-	template <class T>
-	void QuadTree<T>::Remove(QuadTree<T>* origin, T* node)
-	{
-		// TODO:
-	}
-
-	template <class T>
-	Vector<T*> QuadTree<T>::GetNodes(sf::Vector2f position, sf::Vector2f area)
-	{
-		// TODO:
-	}
-
-	template <class T>
-	void QuadTree<T>::AddToStorage(T* node, sf::Vector2f position, sf::Vector2f area)
-	{
-		// TODO:
-	}
-
-	template <class T>
-	void QuadTree<T>::FlattenTree()
-	{
-		// TODO:
-	}
-
-	template <class T>
-	void QuadTree<T>::SplitTree()
-	{
-		// TODO:
-	}
 }
