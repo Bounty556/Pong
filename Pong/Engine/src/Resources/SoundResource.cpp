b970001e1b37008ca294cad935048e8ad74777ab
Bounty556 <jacobmayday@gmail.com>
2021-08-01 15:35:24 -0600

    Added SoundResource

---
+++ Pong/Engine/src/Resources/SoundResource.cpp
@@ -0,0 +1,31 @@
+#include "SoundResource.h"
+
+namespace Soul
+{
+	SoundResource::SoundResource(const char* filePath) :
+		m_Sound(NEW(sf::SoundBuffer))
+	{
+		if (!m_Sound->loadFromFile(filePath))
+		{
+			LOG_ERROR("Failed to load sound from path %s", filePath);
+			m_Sound = nullptr;
+		}
+	}
+
+	SoundResource::SoundResource(SoundResource&& other) noexcept :
+		m_Sound(std::move(other.m_Sound))
+	{
+	}
+
+	SoundResource& SoundResource::operator=(SoundResource&& other) noexcept
+	{
+		m_Sound = std::move(other.m_Sound);
+
+		return *this;
+	}
+
+	const sf::SoundBuffer* SoundResource::GetSound() const
+	{
+		return m_Sound.Raw();
+	}
+}
---
+++ Pong/Engine/src/Resources/SoundResource.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <Resources/IResource.h>
+#include <Memory/UniquePointer.h>
+
+#include <SFML/Audio/SoundBuffer.hpp>
+
+namespace Soul
+{
+	class SOULAPI SoundResource : public IResource
+	{
+	public:
+		SoundResource(const char* filePath);
+
+		SoundResource(const SoundResource& other) = delete;
+		SoundResource(SoundResource&& other) noexcept;
+
+		SoundResource& operator=(const SoundResource& other) = delete;
+		SoundResource& operator=(SoundResource&& other) noexcept;
+
+		const sf::SoundBuffer* GetSound() const;
+
+	private:
+		UniquePointer<sf::SoundBuffer> m_Sound;
+	};
+}
