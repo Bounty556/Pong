cffb70e497183af4e7fb618994c1af20f7e0d6e2
Bounty556 <jacobmayday@gmail.com>
2021-08-01 15:39:11 -0600

    Added SoundResourceTest

--- Pong/TestBed/src/Tests/ResourceManagerTests.cpp
+++ Pong/TestBed/src/Tests/ResourceManagerTests.cpp
@@ -3,6 +3,7 @@
 #include <Memory/MemoryManager.h>
 #include <Resources/ResourceManager.h>
 #include <Resources/TextureResource.h>
+#include <Resources/SoundResource.h>
 
 #include "../TestMacros.h"
 
@@ -25,7 +26,26 @@ void TextureResourceTest()
 	END_MEMORY_CHECK();
 }
 
+void SoundResourceTest()
+{
+	START_MEMORY_CHECK();
+
+	Soul::ResourceManager::LoadResource<Soul::SoundResource>("res/sound.ogg", "Sound");
+
+	Soul::SoundResource* resource = Soul::ResourceManager::GetResource<Soul::SoundResource>("Sound");
+
+	ASSERT_NOT_EQUAL(resource, nullptr, "Failed to load and retrieve resource.");
+
+	Soul::ResourceManager::UnloadResource("Sound");
+
+	Soul::SoundResource* resource2 = Soul::ResourceManager::GetResource<Soul::SoundResource>("Sound");
+
+	ASSERT_EQUAL(resource2, nullptr, "Failed to delete resource.");
+
+	END_MEMORY_CHECK();
+}
 void ResourceManagerTests::RunAllTests()
 {
 	RUN_TEST(TextureResourceTest);
+	RUN_TEST(SoundResourceTest);
 }
